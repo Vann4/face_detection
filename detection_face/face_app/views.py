123514b134cc801ac879125c200f5a27908372e4
Vann4 <1vetlan1@mail.ru>
2024-01-17 10:13:26 +0300

    adding date_user and 404 exception handler

--- detection_face/face_app/views.py
+++ detection_face/face_app/views.py
@@ -1,6 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from face_app.models import *
 
 
 def index(request):
     return render(request, 'face_app/index.html')
+
+
+def date_user(request, user_id):
+    return render(request, 'face_app/date.html')
+
+
+def page_not_found(request, exception):
+    return HttpResponse('<h1>Страница не найдена!</h1>')
