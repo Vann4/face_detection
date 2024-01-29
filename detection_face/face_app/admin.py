30801dddd68188e9c2cde349ce8d4af66271c4bd
Comp22023 <comp22023@mail.ru>
2024-01-29 12:35:18 +0300

    registration of models

--- detection_face/face_app/admin.py
+++ detection_face/face_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from face_app.models import User
+from face_app.models import User, Data_user
+
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Data_user)
