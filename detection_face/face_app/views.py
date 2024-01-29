9dc93c0a55bb4c4471f4042ab96507dd2d71740f
Vann4 <1vetlan1@mail.ru>
2024-01-29 21:48:42 +0300

    test

--- detection_face/face_app/views.py
+++ detection_face/face_app/views.py
@@ -25,23 +25,35 @@ import pickle
 
 def index(request):
     username = User.objects.all()
-    # print(compare_faces("dataset/regina_1.jpg", "dataset_from_video/regina_2.jpg"))
-    img1 = face_recognition.load_image_file("face_app/dataset/regina_1.jpg")
-    img1_encodings = face_recognition.face_encodings(img1)[0]
-    # print(img1_encodings)
-
-    img2 = face_recognition.load_image_file("face_app/dataset/regina_2.jpg")
-    img2_encodings = face_recognition.face_encodings(img2)[0]
-
-    result = face_recognition.compare_faces([img1_encodings], img2_encodings)
+    # # print(compare_faces("dataset/regina_1.jpg", "dataset_from_video/regina_2.jpg"))
+    # img1 = face_recognition.load_image_file("face_app/dataset/regina_1.jpg")
+    # img1_encodings = face_recognition.face_encodings(img1)[0]
+    # # print(img1_encodings)
+    #
+    # img2 = face_recognition.load_image_file("face_app/dataset/regina_2.jpg")
+    # img2_encodings = face_recognition.face_encodings(img2)[0]
+    #
+    # result = face_recognition.compare_faces([img1_encodings], img2_encodings)
     # print(result)
 
-    if result[0]:
-        print("Welcome to the club! :*")
-    else:
-        print("Sorry, not today... Next!")
+    # if result[0]:
+    #     print("Welcome to the club! :*")
+    # else:
+    #     print("Sorry, not today... Next!")
+
+    # context = {}
+
+    # if result[0]:
+    #     context['welcome_text'] = "Welcome to the club!"
+    #     # aa = {'a': 'fgfg'}
+    #     # aa = 'fg'
+    # else:
+    #     context['sorry_text'] = "Sorry, not today..."
+
     data = {
         'username': username,
+        # 'context': context,
+        # 'aa': aa,
     }
     return render(request, 'face_app/index.html', data)
 
