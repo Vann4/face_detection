6167e7d586bc9e38c8e569aa2ea471ccd90e3f75
Vann4 <1vetlan1@mail.ru>
2024-01-30 01:22:56 +0300

    Creating an authorization and exit path

--- detection_face/face_app/urls.py
+++ detection_face/face_app/urls.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('date_user/<int:user_id>/', views.date_user, name='date_user'),
+    path('login/', views.LoginUser.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('registration/', views.registration, name='registration'),
 ]
