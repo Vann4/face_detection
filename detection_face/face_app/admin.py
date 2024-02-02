17b3ba13e94cf25958dd18a50a9f461057a4dd36
Vann4 <1vetlan1@mail.ru>
2024-02-03 00:21:54 +0300

    feedback model registration

--- detection_face/face_app/admin.py
+++ detection_face/face_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from face_app.models import User, Data_user
+from .models import User, Data_user, Feedback
 
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Data_user)
+admin.site.register(Feedback)
