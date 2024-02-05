d37662c355524df20edc70250614ecac7ade7615
Vann4 <1vetlan1@mail.ru>
2024-02-05 20:33:02 +0300

    closing a modal window by pressing esc

--- detection_face/face_app/static/js/main.js
+++ detection_face/face_app/static/js/main.js
@@ -5,3 +5,9 @@ document.getElementById("close-button").addEventListener("click", function() {
 document.getElementById("popup_feedback").addEventListener("click", function() {
   document.getElementById("popup_feedback_class").style.display = "block";
 });
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("popup_feedback_class").style.display = "none";
+    }
+});
