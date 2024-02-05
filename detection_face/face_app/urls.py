308a595e3ab80b03200ffbdf1c5e5dcdab0a2a21
Vann4 <1vetlan1@mail.ru>
2024-02-05 21:48:50 +0300

    Added a path for uploading files

--- detection_face/face_app/urls.py
+++ detection_face/face_app/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('date_user/<int:user_id>/', views.date_user, name='date_user'),
+    # path('date_user/<int:user_id>/', views.date_user, name='date_user'),
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('registration/', views.registration, name='registration'),
+    path('upload_face/', views.upload_face, name='upload_face'),
 ]
