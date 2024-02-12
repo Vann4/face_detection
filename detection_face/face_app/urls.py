a2c83bb1c51468146741e24070a3b3154f43ee13
Vann4 <1vetlan1@mail.ru>
2024-02-12 22:20:33 +0300

    imported settings and static

--- detection_face/face_app/urls.py
+++ detection_face/face_app/urls.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('date_user/<int:user_id>/', views.date_user, name='date_user'),
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('registration/', views.registration, name='registration'),
-    path('working_with_images/', views.working_with_images, name='working_with_images'),
-]
+    path('working_with_images/<int:users_id>/', views.working_with_images, name='working_with_images'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
