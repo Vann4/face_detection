33653774706363eaee8fd9fab22d5dde7aca2aa5
Vann4 <1vetlan1@mail.ru>
2024-02-12 22:19:24 +0300

    Changed the path name for saving images

--- detection_face/detection_face/settings.py
+++ detection_face/detection_face/settings.py
@@ -127,8 +127,8 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "face_app/static")
 ]
 
-MEDIA_URL = 'img/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'face_app/static/img/face_trim')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'face_app/media/')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
