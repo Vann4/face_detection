56749acefd9f26bc2f929929f3a65333a6568538
Comp22023 <comp22023@mail.ru>
2024-02-16 17:18:26 +0300

    Adaptation

--- detection_face/face_app/static/js/main.js
+++ detection_face/face_app/static/js/main.js
@@ -2,9 +2,14 @@ document.getElementById("close-button").addEventListener("click", function() {
   document.getElementById("popup_feedback_class").style.display = "none";
 });
 
-document.getElementById("popup_feedback").addEventListener("click", function() {
-  document.getElementById("popup_feedback_class").style.display = "block";
-});
+const popup_feedback = document.querySelector("#popup_feedback");
+
+if (popup_feedback) {
+    popup_feedback.addEventListener("click", function() {
+        document.getElementById("popup_feedback_class").style.display = "block";
+    });
+} else {
+}
 
 window.addEventListener('keydown', (e) => {
     if (e.key === "Escape") {
@@ -18,24 +23,32 @@ const popup = document.querySelector("#popup");
 const body = document.body;
 
 // Клонируем меню, чтобы задать свои стили для мобильной версии
-//const menu = document.querySelector("#menu").cloneNode(1);
+const menu = document.querySelector("#menu").cloneNode(1);
+const auth = document.querySelector(".auth").cloneNode(1);
 
 // При клике на иконку hamb вызываем ф-ию hambHandler
 hamb.addEventListener("click", hambHandler);
 
 // Выполняем действия при клике ..
 function hambHandler(e) {
-  e.preventDefault();
-  // Переключаем стили элементов при клике
-  popup.classList.toggle("open");
-  hamb.classList.toggle("active");
-  body.classList.toggle("noscroll");
-  renderPopup();
+    e.preventDefault();
+    // Переключаем стили элементов при кликеo
+    popup.classList.toggle("open");
+    hamb.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    renderPopup();
 }
 
 // Здесь мы рендерим элементы в наш попап
 function renderPopup() {
-  popup.appendChild(menu);
+    popup.appendChild(menu);
+    popup.appendChild(auth);
+    buttons = document.querySelectorAll("#popup_feedback")
+    buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        document.getElementById("popup_feedback_class").style.display = "block";
+        });
+    });
 }
 
 // Код для закрытия меню при нажатии на ссылку
@@ -43,12 +56,12 @@ const links = Array.from(menu.children);
 
 // Для каждого элемента меню при клике вызываем ф-ию
 links.forEach((link) => {
-  link.addEventListener("click", closeOnClick);
+    link.addEventListener("click", closeOnClick);
 });
 
 // Закрытие попапа при клике на меню
 function closeOnClick() {
-  popup.classList.remove("open");
-  hamb.classList.remove("active");
-  body.classList.remove("noscroll");
+    popup.classList.remove("open");
+    hamb.classList.remove("active");
+    body.classList.remove("noscroll");
 }
