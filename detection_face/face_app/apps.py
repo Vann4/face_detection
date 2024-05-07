d674ef4de04203379e46655acf12b63bb05ddc33
Vann4 <1vetlan1@mail.ru>
2024-05-07 20:17:20 +0300

    Заменил почту на е-майл, изменил имя приложения в панеле админа, убрал таблицу с изображениями и группы в админ панеле, изменил тип на числовой

--- detection_face/face_app/apps.py
+++ detection_face/face_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class FaceAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'face_app'
+    verbose_name = 'Пользователи и обратная связь'
