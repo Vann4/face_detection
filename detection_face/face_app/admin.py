2fd222a5202080c5c2938ba364f6bb6a4b897cf8
Vann4 <1vetlan1@mail.ru>
2024-06-02 19:07:37 +0300

    убрал пользователей в админ-панели

--- detection_face/face_app/admin.py
+++ detection_face/face_app/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin
-from .models import User, Feedback
-
-admin.site.register(User, UserAdmin)
+from .models import Feedback
 
 
 class FeedbackAdmin(admin.ModelAdmin):
